62b1b9f1865962f88237f056c843b6cfec909f42
SulthanRaghib <raghib.smp@gmail.com>
2025-08-25 14:50:01 +0700

    Refactor string manipulation function and update comments for clarity

--- string.go
+++ string.go
@@ -1,16 +1,14 @@
-// MANIPULASI STRING
+// 06 Manipulasi String
 package main
 
 import "fmt"
 
-func main() {
+func latihanString() {
 	fmt.Println("Sulthan Raghib Fillah")           // mencetak nama
 	fmt.Println("Sulthan Raghib" + " " + "Fillah") // menggabungkan string dengan tanda +
 	fmt.Println(len("Sulthan Raghib Fillah"))      // menghitung jumlah panjang string
-	fmt.Println("Sulthan Raghib Fillah"[0])        // mengambil karakter pertama dari string (result:83) golang mengambil byte dari huruf tersebut
-	fmt.Println("Sulthan Raghib Fillah"[8:14])     // mengambil karakter dari index 8 sampai 14
-	fmt.Println("Sulthan Raghib Fillah"[8:])       // mengambil karakter dari index 8 sampai akhir
-	fmt.Println("Sulthan Raghib Fillah"[8:])       // mengambil karakter dari index 8 sampai akhir
-	fmt.Println("Sulthan Raghib Fillah"[:7])       // mengambil karakter dari index 0 sampai 7
-
+	fmt.Println("Sulthan Raghib Fillah"[0])        // mengambil karakter pertama (byte)
+	fmt.Println("Sulthan Raghib Fillah"[8:14])     // ambil karakter index 8-13
+	fmt.Println("Sulthan Raghib Fillah"[8:])       // ambil karakter index 8 sampai akhir
+	fmt.Println("Sulthan Raghib Fillah"[:7])       // ambil karakter index 0-6
 }
